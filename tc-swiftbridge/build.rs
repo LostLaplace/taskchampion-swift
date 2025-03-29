6fa33072b7aaefafec0bda7b1066c6a1a0e79b11
Cameron Johnson <cam.j.johnson@icloud.com>
2025-03-28 20:21:48 -0500

    Add BUILD_SWIFT environment switch to build
    Sometimes it can be hard to read the errors generated by swift-bridge.
    In these cases, it can be better to allow the rust compiler to give more
    useful errors. Change the default build configuration to not try to generate
    the swift FFI code. When you're ready to generate the code, set BUILD_SWIFT
    environment variable to 1.

--- tc-swiftbridge/build.rs
+++ tc-swiftbridge/build.rs
@@ -1,13 +1,18 @@
 use std::path::PathBuf;
+use std::env;
 
 fn main() {
-    let out_dir = PathBuf::from("./generated");
+    let build_bridge = env::var("BUILD_SWIFT").map(|v| v == "1").unwrap_or(false);
 
-    let bridges = vec!["src/lib.rs", "src/task.rs"];
-    for path in &bridges {
-        println!("cargo:rerun-if-changed={}", path);
+    if build_bridge {
+        let out_dir = PathBuf::from("./generated");
+
+        let bridges = vec!["src/lib.rs", "src/task.rs"];
+        for path in &bridges {
+            println!("cargo:rerun-if-changed={}", path);
+        }
+
+        swift_bridge_build::parse_bridges(bridges)
+            .write_all_concatenated(out_dir, env!("CARGO_PKG_NAME"));
     }
-
-    swift_bridge_build::parse_bridges(bridges)
-        .write_all_concatenated(out_dir, env!("CARGO_PKG_NAME"));
 }
